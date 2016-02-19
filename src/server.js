47a230528bb13890e6161bdf9005d496038111d2
Enja Reyes <enja.reyes@gmail.com>
2016-02-18 21:12:32 -0600

    cleanup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import 'babel-polyfill';
 import path from 'path';
 import express from 'express';
@@ -19,17 +10,14 @@ import { port } from './config';
 
 const server = global.server = express();
 
-//
 // Register Node.js middleware
 // -----------------------------------------------------------------------------
 server.use(express.static(path.join(__dirname, 'public')));
 
-//
 // Register API middleware
 // -----------------------------------------------------------------------------
 server.use('/api/content', require('./api/content').default);
 
-//
 // Register server-side rendering middleware
 // -----------------------------------------------------------------------------
 server.get('*', async (req, res, next) => {
@@ -56,7 +44,6 @@ server.get('*', async (req, res, next) => {
   }
 });
 
-//
 // Launch the server
 // -----------------------------------------------------------------------------
 server.listen(port, () => {
