d898f39c12e3d79346c562c2a8904a6581fe0165
Enja Reyes <enja.reyes@gmail.com>
2016-02-18 21:27:17 -0600

    cleanup removed privacy

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,6 @@ class Footer extends Component {
           <span className={s.spacer}>·</span>
           <Link className={s.link} to="/">Home</Link>
           <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">Privacy</Link>
           <span className={s.spacer}>·</span>
           <Link className={s.link} to="/not-found">Not Found</Link>
         </div>
