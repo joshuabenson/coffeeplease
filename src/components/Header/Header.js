a53e6bca4225122330b58b4e2d24ce3e910fcc6e
Enja Reyes <enja.reyes@gmail.com>
2016-02-18 21:50:58 -0600

    some styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,12 +12,11 @@ class Header extends Component {
         <div className={s.container}>
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
-            <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <img src={require('./logo-small.png')} width="80" height="80" alt="React" />
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Coffee Please</h1>
+            <p className={s.bannerDesc}>Getting you your daily cup</p>
           </div>
         </div>
       </div>
